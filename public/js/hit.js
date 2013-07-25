4f98b8002b6d02df8ad200dfb91fc15eedac3d14
Yuri Gorokhov <yurigorokhov@gmail.com>
2013-07-24 22:52:50 -0700

    Adding view venue route

--- public/js/hit.js
+++ public/js/hit.js
@@ -2,6 +2,11 @@
 
     // Login
     var loginCtrl = function($scope, $routeParams) {
+        var currentUser = Hit.User.getCurrent();
+        if(currentUser) {
+            window.location.href = '/#/venues';
+            return;
+        }
         $scope.failed = false;
         $scope.login = function(user) {
             $scope.failed = false;
@@ -16,11 +21,60 @@
         };
     };
 
+    // Venue searchCtrl
+    var venueSearchCtrl = function($rootScope, $routeParams) {
+        var scope = $rootScope;
+        scope.venues = [];
+        scope.searching = false;
+        scope.error = false;
+        var timeout = null;
+        scope.search = function(searchtext) {
+            scope.searching = true;
+            scope.error = false;
+            scope.$apply();
+            clearTimeout(timeout);
+            timeout = setTimeout(function() { 
+                scope.venues = [];
+                if(!_(searchtext).isEmpty()) {
+                    Hit.Venue.searchByName(searchtext).done(function(venues) {
+                        scope.searching = false;
+                        scope.venues = venues;
+                        scope.$apply();
+                    }).fail(function() {
+                        scope.error = true;
+                        scope.searching = false;
+                        scope.$apply();
+                    });
+                } else {
+                    scope.$apply();
+                }
+            }, 500);
+        }; 
+        scope.logout = function() {
+            Hit.User.logOut();
+            window.location.href = '/#/login';
+        };
+    };
+
+    var viewVenueCtrl = function($rootScope, $routeParams) {
+        var scope = $rootScope;
+        var venueid = $routeParams.venueid;
+        scope.error = null;
+        Hit.Venue.getById(venueid).done(function(venue) {
+            scope.venue = venue;
+            scope.$apply();
+        }).fail(function(venue, error) {
+            scope.error = error.message;
+            scope.$apply();
+        });
+    };
+
     angular.module('hit', []).
         config(['$routeProvider', function($routeProvider) {
             $routeProvider.
-            when('/login', {templateUrl: 'partials/login.html',   controller: loginCtrl}).
-            when('/venues', {templateUrl: 'partials/venues.html', controller: loginCtrl}).
+            when('/login', {templateUrl: 'partials/login.html', controller: loginCtrl}).
+            when('/venues', {templateUrl: 'partials/venues.html', controller: venueSearchCtrl}).
+            when('/venues/:venueid', {templateUrl: 'partials/viewvenue.html', controller: viewVenueCtrl}).
             otherwise({redirectTo: '/login'});
     }]);
 
