378c0d34b0eda5218657dceb50a8c861d1e85ed3
Yuri Gorokhov <yurigorokhov@gmail.com>
2013-07-27 15:32:27 -0700

    Adding view venue

--- public/js/hit.js
+++ public/js/hit.js
@@ -1,5 +1,7 @@
 (function($) {
 
+    
+
     // Login
     var loginCtrl = function($scope, $routeParams) {
         var currentUser = Hit.User.getCurrent();
@@ -21,15 +23,35 @@
         };
     };
 
+    // Register
+    var registerCtrl = function($scope, $routeParams) {
+        var currentUser = Hit.User.getCurrent();
+        if(currentUser) {
+            window.location.href = '/#/venues';
+            return;
+        }
+        $scope.register = function(user) {
+            $scope.failed = null;
+            if($scope.registerForm.$valid) {
+                Hit.User.signUp(user).done(function() {
+                    window.location.href = '/#/venues';
+                }).fail(function(user, error) {
+                    $scope.failed = error.message || 'Registration failed. Please try again.';
+                    $scope.$apply();
+                });
+            }
+        };
+    };
+
     // Venue searchCtrl
     var venueSearchCtrl = function($rootScope, $routeParams) {
 
         // login check
         if(!Hit.User.getCurrent()) {
             window.location.href = '/#/login';
-        }
-
+        } 
         var scope = $rootScope;
+        scope.user = Hit.User.getCurrent();
         scope.venues = [];
         scope.searching = false;
         scope.error = false;
@@ -37,10 +59,10 @@
         scope.search = function(searchtext) {
             scope.searching = true;
             scope.error = false;
+            scope.venues = [];
             scope.$apply();
             clearTimeout(timeout);
-            timeout = setTimeout(function() { 
-                scope.venues = [];
+            timeout = setTimeout(function() {
                 if(!_(searchtext).isEmpty()) {
                     Hit.Venue.searchByName(searchtext).done(function(venues) {
                         scope.searching = false;
@@ -52,6 +74,7 @@
                         scope.$apply();
                     });
                 } else {
+                    scope.searching = false;
                     scope.$apply();
                 }
             }, 500);
@@ -62,14 +85,15 @@
         };
     };
 
+    // View Venue
     var viewVenueCtrl = function($rootScope, $routeParams) {
 
         // login check
         if(!Hit.User.getCurrent()) {
             window.location.href = '/#/login';
         }
-
         var scope = $rootScope;
+        scope.user = Hit.User.getCurrent();
         var venueid = $routeParams.venueid;
         scope.error = null;
         Hit.Venue.getById(venueid).done(function(venue) {
@@ -85,6 +109,7 @@
         config(['$routeProvider', function($routeProvider) {
             $routeProvider.
             when('/login', {templateUrl: 'partials/login.html', controller: loginCtrl}).
+            when('/register', {templateUrl: 'partials/register.html', controller: registerCtrl}).
             when('/venues', {templateUrl: 'partials/venues.html', controller: venueSearchCtrl}).
             when('/venues/:venueid', {templateUrl: 'partials/viewvenue.html', controller: viewVenueCtrl}).
             otherwise({redirectTo: '/login'});
